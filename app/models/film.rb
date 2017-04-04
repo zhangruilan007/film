a853429383401f726b77d60bb1df036026e871a2
张瑞兰 <zhangruilan007@gmail.com>
2017-04-04 21:19:30 +0800

    add post and hook film/user relation

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,5 @@
 class Film < ApplicationRecord
   belongs_to :user
+  has_many :reviews
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :films       
+  has_many :films
+  has_many :reviews     
 end
