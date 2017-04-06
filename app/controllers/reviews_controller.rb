268aa750d906880fb41e516a09ac02cfa967d6cc
张瑞兰 <zhangruilan007@gmail.com>
2017-04-06 16:12:49 +0800

    implement favorite film can write a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_film_and_check_favorite, only: [:new, :create]
 
   def new
     @film = Film.find(params[:film_id])
@@ -18,6 +19,13 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
+
+def find_film_and_check_favorite
+  @film = Film.find(params[:film_id])
+  if !current_user.is_member_of?(@film)
+    redirect_to film_path(@film), alert:"You have no permission!"
+  end
+end
 
 
   private
